a0c8fa87f9c4139681cb15b0884a5e9db2d4bf02
Vanessa <vanessa.choque@tecsup.edu.pe>
2023-09-29 14:55:16 -0500

    Initial Commit

---
+++ TablesChoque/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TablesChoque
+//
+//  Created by MAC45 on 29/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
